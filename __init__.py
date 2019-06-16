2c3c221343474459dab83c4ea6feede0c9db65f6
Noamshmueli <47123652+Noamshmueli@users.noreply.github.com>
2019-06-16 16:25:04 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.secret_key = 'many random bytes'
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '10.0.3.208'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'crud'
